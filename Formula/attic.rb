5be9d7cc5a9a0938a17bf6cf8c66c8e75002e969
Tijs Teulings <hello@tijs.org>
2026-03-13 19:24:11 +0100

    Add attic formula

---
+++ Formula/attic.rb
@@ -0,0 +1,28 @@
+class Attic < Formula
+  desc "Back up your iCloud Photos library to S3-compatible storage"
+  homepage "https://github.com/tijs/attic"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tijs/attic/releases/download/v#{version}/attic-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/tijs/attic/releases/download/v#{version}/attic-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "attic-aarch64-apple-darwin" => "attic"
+    else
+      bin.install "attic-x86_64-apple-darwin" => "attic"
+    end
+  end
+
+  test do
+    assert_match "attic", shell_output("#{bin}/attic --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Homebrew Tap
+
+Homebrew formulae for [tijs](https://github.com/tijs) projects.
+
+## Install
+
+```bash
+brew tap tijs/tap
+brew install attic
+```
+
+Or directly:
+
+```bash
+brew install tijs/tap/attic
+```
+
+## Formulae
+
+| Formula | Description |
+|---------|-------------|
+| [attic](https://github.com/tijs/attic) | Back up your iCloud Photos library to S3-compatible storage |
