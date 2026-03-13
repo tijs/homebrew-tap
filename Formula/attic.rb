8fcc859a17308e660c8c52e58f2afefd3721b94e
Tijs Teulings <hello@tijs.org>
2026-03-13 19:28:26 +0100

    attic 0.1.0

--- Formula/attic.rb
+++ Formula/attic.rb
@@ -7,10 +7,10 @@ class Attic < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tijs/attic/releases/download/v#{version}/attic-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "cc5f88bf90e0a44fabd76e1aaf743563922874243a48904d33f13c4b3142fe81"
     else
       url "https://github.com/tijs/attic/releases/download/v#{version}/attic-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "03903285e2c3bf3dab50ca6d7fba4a7643f664f8a317a826d3df83717ada4529"
     end
   end
 
