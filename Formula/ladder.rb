a7ec4446912c4ade85917deb41b11152b0feae51
Tijs Teulings <hello@tijs.org>
2026-03-21 18:21:12 +0100

    ladder 0.3.3

--- Formula/ladder.rb
+++ Formula/ladder.rb
@@ -1,13 +1,13 @@
 class Ladder < Formula
   desc "PhotoKit export helper for iCloud Photos backup"
   homepage "https://github.com/tijs/ladder"
-  version "0.3.2"
+  version "0.3.3"
   license "Apache-2.0"
 
   depends_on arch: :arm64
 
   url "https://github.com/tijs/ladder/releases/download/v#{version}/ladder-#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "512f46c9440d8f3504597077065e066a4f6963df7b564c694fc21aff80d56153"
+  sha256 "fa3c93aa16f9265e603047653dc1e0516938ffbb8bb716be3248492bd41307a1"
 
   def install
     bin.install "ladder-aarch64-apple-darwin" => "ladder"
