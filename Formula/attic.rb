625f15c36ceccce4a0b8db764c04667214a02cb2
Tijs Teulings <hello@tijs.org>
2026-03-21 22:39:53 +0100

    Fix attic SHA to match CI-built binary

--- Formula/attic.rb
+++ Formula/attic.rb
@@ -7,7 +7,7 @@ class Attic < Formula
   depends_on arch: :arm64
 
   url "https://github.com/tijs/attic/releases/download/v#{version}/attic-#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "cd19713e7ba870a21b76aa3dc5c070004b2c2ec115fe202e4b9ba7a9544d52ce"
+  sha256 "e034da1ce529d03ac1778a95fad15f7364b6fb66f9be0c6c2b3f8de279ae6516"
 
   def install
     bin.install "attic"
