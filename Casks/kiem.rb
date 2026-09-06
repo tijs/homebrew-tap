9dd15548d0e0e6a2160c4ee9b9e722f4f9540804
Tijs Teulings <tijs@users.noreply.github.com>
2026-09-06 12:50:29 +0200

    chore: update Kiem cask to 0.4.1

--- Casks/kiem.rb
+++ Casks/kiem.rb
@@ -1,6 +1,6 @@
 cask "kiem" do
-  version "0.4.0"
-  sha256 "9a2f31b0dc5720892cbf7168ca7dc51d9b448893065ecfdabe4fbedfebf0523e"
+  version "0.4.1"
+  sha256 "8811043aa644d5152bc61bdfebb736c3eb53a3009a112df34219b470a27f0d59"
 
   url "https://github.com/tijs/kiem/releases/download/v#{version}/Kiem-#{version}.dmg"
   name "Kiem"
